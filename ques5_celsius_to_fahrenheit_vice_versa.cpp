d44d148d49aaa24cd92066c12e6ea9b7e5c87191
kanakchauhan2509 <kanakchauhan2509@gmail.com>
2026-06-22 20:49:48 +0530

    Create ques5_celsius_to_fahrenheit_vice_versa.cpp

---
+++ ques5_celsius_to_fahrenheit_vice_versa.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+  float celsius, fahrenheit;
+int choice;
+printf("1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\nEnter choice (1 or 2):");
+scanf("%d",&choice);
+if(choice == 1){
+printf("Enter temperature in celsius:");
+scanf("%f",&celsius);
+fahrenheit = (celsius * 9 / 5) + 32;
+printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
+} else if ( choice == 2) {
+printf("Enter temperature in Fahrenheit:");
+scanf("%f", &fahrenheit);
+celsius = (fahrenheit - 32) * 5 / 9;
+printf("Temperature in Celsius: %.2f\n", celsius);
+} else {
+printf("Invalid choice!\n");
+}
+return 0;
+}
