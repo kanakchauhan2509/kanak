db5572ca639fd012827925342337c730cc8e9905
kanakchauhan2509 <kanakchauhan2509@gmail.com>
2026-06-25 23:45:09 +0530

    Implement number pyramid pattern in C++

---
+++ ques35_number_pyramid.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+  int rows = 5;
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= i; j++) {
+      std::cout << j << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
